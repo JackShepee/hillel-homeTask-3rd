ce682d768b8bfdbc896b1d3c589a1a987d414f7f
JackShepee <salamakhaviktor@gmail.com>
2023-05-25 22:08:24 +0300

    1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const hours = prompt("Введіт кількість годин");
+const hours = prompt("Введіть кількість годин");
 
 if (isNaN(hours) || hours < 0 || !hours) {
   alert("Введіть вірну кількість годин!");
